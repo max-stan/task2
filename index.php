9b7a6b6ad9591561eaea8da7da7b37792b95683e
maxstan <maxstan@gmail.com>
2024-11-13 21:45:14 +0200

    Remove redudant recursion, add linked array

--- index.php
+++ index.php
@@ -10,39 +10,29 @@ $stmt = $mysqli->query('SELECT * FROM categories');
 $categories = $stmt->fetch_all(MYSQLI_ASSOC);
 
 $items = [];
-$tree = [];
+$parents = [];
 
 foreach ($categories as $category) {
-    $category['children'] = [];
-    $items[$category['categories_id']] = $category;
+    $categoriesId = $category['categories_id'];
+
+    $parents[$categoriesId] = $category['parent_id'];
+    $items[$categoriesId] = $categoriesId;
 }
 
 foreach ($items as $id => &$item) {
-    if ($item['parent_id'] == 0) {
+    $parentId = $parents[$id];
+
+    if ($parentId == 0) {
         $tree[0][] = &$item;
     } else {
-        $items[$item['parent_id']]['children'][] = &$item;
+        if (!is_array($items[$parentId])) {
+            $items[$parentId] = [];
+        }
+
+        $items[$parentId][$id] = &$item;
     }
 }
 
-function formatTree($node) {
-    if (empty($node['children'])) {
-        return $node['categories_id'];
-    }
-
-    $result = [];
-    foreach ($node['children'] as $child) {
-        $result[$child['categories_id']] = formatTree($child);
-    }
-    return $result;
-}
-
-$formattedTree = [];
-foreach ($tree as $parent_id => $nodes) {
-    foreach ($nodes as $node) {
-        $formattedTree[$parent_id][$node['categories_id']] = formatTree($node);
-    }
-}
 
 echo "Структура дерева :\n";
-print_r($formattedTree[0]);
+print_r($items);
